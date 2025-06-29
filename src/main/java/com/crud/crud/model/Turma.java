e2459d0cf4650e7965265882f9f54b063c35d7c0
gabirote <159089736+gabirote@users.noreply.github.com>
2025-06-29 13:04:34 -0300

    [FIX] Correção Entidade

--- src/main/java/com/crud/crud/model/Turma.java
+++ src/main/java/com/crud/crud/model/Turma.java
@@ -5,11 +5,36 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
+
 @Entity
 public class Turma {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
+
     private String nome;
     private String periodo;
+
+
+    public long getID() {
+        return ID;
+    }
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
