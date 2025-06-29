c3817b8773faff2170f629f82794de282fd4d624
feliquisds <mail@feliquisds.one>
2025-06-29 12:24:41 -0300

    updated Aluno request mapping

--- src/main/java/com/crud/crud/controller/AlunoController.java
+++ src/main/java/com/crud/crud/controller/AlunoController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.crud.model.Aluno;
 import com.crud.crud.service.AlunoService;
 
 @RestController
+@RequestMapping("/aluno")
 public class AlunoController {
 
     @Autowired
@@ -21,13 +23,13 @@ public class AlunoController {
         this.alunoService = alunoService;
     }
 
-    @PostMapping("/aluno/save")
+    @PostMapping("/save")
     public Aluno save(@RequestBody Aluno a) {
         Aluno novo_aluno = alunoService.save(a);
         return novo_aluno;
     }
 
-    @GetMapping("/aluno/findAll")
+    @GetMapping("/findAll")
     public List<Aluno> findAll() {
         return (List<Aluno>) alunoService.findAll();
     }
