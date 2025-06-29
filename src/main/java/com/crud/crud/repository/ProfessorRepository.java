d3a74e7db945f451a9120e9542b78dbaa1951922
evelynvic23 <evelynvic2310@gmail.com>
2025-06-29 13:34:30 -0300

    Fix Adicionando importação

--- src/main/java/com/crud/crud/repository/ProfessorRepository.java
+++ src/main/java/com/crud/crud/repository/ProfessorRepository.java
@@ -1,7 +1,11 @@
 package com.crud.crud.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.crud.crud.model.Professor;
+
+@Repository
 
 public interface ProfessorRepository extends CrudRepository<Professor, Long> {
 }
